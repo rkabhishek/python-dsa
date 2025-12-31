3df4eb501c7b1c1d50b083e2426f17b976ff71c5
Abhishek Kaushik <akaushik079@gmail.com>
2025-12-31 14:54:45 +0530

    LC-2011: add solution

---
+++ 2011_final_value_of_variable.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: list[str]) -> int:
+        ans = 0
+        for op in operations:
+            if op[1] == "+":
+                ans += 1
+            else:
+                ans -= 1
+
+        return ans
