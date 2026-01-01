2b088eecc8b0ce6d185e9cab17c7a6f15ea7298f
Abhishek Kaushik <akaushik079@gmail.com>
2026-01-01 14:53:13 +0530

    LC-1431: replace loop with list comprehension

--- 1431_kids_with_greatest_number_of_candies.py
+++ 1431_kids_with_greatest_number_of_candies.py
@@ -1,7 +1,5 @@
 class Solution:
     def kidsWithCandies(self, candies: list[int], extraCandies: int) -> list[bool]:
         maxCandies = max(candies)
-        result = []
-        for candy in candies:
-            result.append(candy + extraCandies >= maxCandies)
+        result = [candy + extraCandies >= maxCandies for candy in candies]
         return result
