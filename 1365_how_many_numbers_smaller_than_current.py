34eb000b0d77c962d36c8b75ab6fdc30e4c91d10
Abhishek Kaushik <akaushik079@gmail.com>
2026-01-01 15:38:48 +0530

    LC-1365: simplify inner loop using generator expression

--- 1365_how_many_numbers_smaller_than_current.py
+++ 1365_how_many_numbers_smaller_than_current.py
@@ -2,9 +2,6 @@ class Solution:
     def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
         result = []
         for x in nums:
-            small_count = 0
-            for y in nums:
-                if x > y:
-                    small_count += 1
+            small_count = sum(1 for y in nums if x > y)
             result.append(small_count)
         return result
