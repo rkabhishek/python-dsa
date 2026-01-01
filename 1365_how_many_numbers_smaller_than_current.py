0cf4410ef3b63a0370e850df0104fc7dd3ce0961
Abhishek Kaushik <akaushik079@gmail.com>
2026-01-01 21:53:25 +0530

    LC-1365: add optimised solution using sorting and map

--- 1365_how_many_numbers_smaller_than_current.py
+++ 1365_how_many_numbers_smaller_than_current.py
@@ -5,3 +5,19 @@ class Solution:
             small_count = sum(1 for y in nums if x > y)
             result.append(small_count)
         return result
+
+    def smallerNumbersThanCurrentUsingSort(self, nums: list[int]) -> list[int]:
+        sorted_nums = sorted(nums)
+        smaller_than_map = {}
+        for i in range(len(sorted_nums)):
+            num = sorted_nums[i]
+            if num not in smaller_than_map:
+                smaller_than_map[num] = i
+
+        result = []
+        for i in range(len(nums)):
+            count = smaller_than_map[nums[i]]
+            result.append(count)
+
+        return result
+
