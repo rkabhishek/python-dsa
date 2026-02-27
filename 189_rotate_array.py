ef98c7c6be2d46239141ac479b5ef2546a6dbd4e
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-27 23:12:30 +0530

    LC-189: implement in-place array rotation by three-reversal technique

--- 189_rotate_array.py
+++ 189_rotate_array.py
@@ -8,3 +8,16 @@ class Solution:
         result.extend(nums[:n-k])
 
         nums[:] = result
+
+    def rotate_(self, nums: list[int], k: int) -> None:
+        n = len(nums)
+        self._reverse_range(nums, 0, n - k - 1)
+        self._reverse_range(nums, n - k, n - 1)
+        self._reverse_range(nums, 0, n - 1)
+
+
+    def _reverse_range(self, nums: list[int], left: int, right: int) -> None:
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
