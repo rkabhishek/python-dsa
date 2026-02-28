995b20dd54b2debb1dfef3dfa81b58b4fdfcb2f8
Abhishek Kaushik <akaushik079@gmail.com>
2026-02-28 15:30:32 +0530

    LC-125: implement palindrome check by skipping non-alphanumeric characters

--- 125_is_palindrome.py
+++ 125_is_palindrome.py
@@ -12,3 +12,21 @@ class Solution:
             right -= 1
 
         return True
+
+    def isPalindromeSkipNonAlnum(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].upper() != s[right].upper():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
